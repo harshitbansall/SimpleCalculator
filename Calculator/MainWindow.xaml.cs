2d04edfb06a6b63aa3fd479429153207c01af644
harshitbansall <harshitbansal587@gmail.com>
2022-02-04 03:29:49 +0530

    Added Buttons UI

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 namespace Calculator
 {
     public partial class MainWindow : Window
@@ -7,6 +12,11 @@ namespace Calculator
         public MainWindow()
         {
             InitializeComponent();
+            //List<int> intNums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //foreach (int num in intNums)
+            //{
+
+            //}
         }
     }
 }
