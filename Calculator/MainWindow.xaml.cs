c05c2db1166960ba7fce34d7fdbf7cdb15e09984
harshitbansall <harshitbansal587@gmail.com>
2022-02-07 04:03:11 +0530

    Added Functions to Buttons

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,22 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Calculator
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            Brush focusColor = Brushes.Yellow;
+            Brush defaultColor = Brushes.Black;
+            string firstQuery = null;
+            Button focusOperation = null;
+
             acButton.Click += (s, e) =>
             {
                 mainTextBox.Text = "0";
+            };
+            pointButton.Click += (s, e) =>
+            {
+                if (mainTextBox.Text.Contains(".") == false)
+                {
+                    mainTextBox.Text = mainTextBox.Text + ".";
+                }
+
             };
 
             List<Button> numericButtons = new List<Button> { zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton };
@@ -36,14 +54,53 @@ namespace Calculator
                 };
             }
 
-            pointButton.Click += (s, e) =>
+            
+            plusButton.Click += (s, e) =>
             {
-                if (mainTextBox.Text.Contains(".") == false)
-                {
-                    mainTextBox.Text = mainTextBox.Text + ".";
-                }
-                
+                focusOperation = plusButton;
+                focusOperation.Background = focusColor;
+                firstQuery = mainTextBox.Text;
+                mainTextBox.Text = "";
             };
+            minusButton.Click += (s, e) =>
+            {
+                focusOperation = minusButton;
+                focusOperation.Background = focusColor;
+                firstQuery = mainTextBox.Text;
+                mainTextBox.Text = "";
+            };
+            multiplyButton.Click += (s, e) =>
+            {
+                focusOperation = multiplyButton;
+                focusOperation.Background = focusColor;
+                firstQuery = mainTextBox.Text;
+                mainTextBox.Text = "";
+            };
+            divideButton.Click += (s, e) =>
+            {
+                focusOperation = divideButton;
+                focusOperation.Background = focusColor;
+                firstQuery = mainTextBox.Text;
+                mainTextBox.Text = "";
+            };
+
+            equalsButton.Click += (s, e) =>
+            {
+                focusOperation.Background = default;
+                if (focusOperation.Content.Equals("+")){ mainTextBox.Text = (Int16.Parse(firstQuery) + Int16.Parse(mainTextBox.Text)).ToString(); }
+                else if (focusOperation.Content.Equals("-")) { mainTextBox.Text = (Int16.Parse(firstQuery) - Int16.Parse(mainTextBox.Text)).ToString(); }
+                else if (focusOperation.Content.Equals("*")) { mainTextBox.Text = (Int16.Parse(firstQuery) * Int16.Parse(mainTextBox.Text)).ToString(); }
+                else if (focusOperation.Content.Equals("/")) { mainTextBox.Text = (Int16.Parse(firstQuery) / Int16.Parse(mainTextBox.Text)).ToString(); }
+            };
+            
+        }
+        private Brush _defaultColor;
+        public Brush defaultColor { get { return _defaultColor; } set { _defaultColor = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
     }
 }
